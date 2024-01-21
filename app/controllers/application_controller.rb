b2d8015d01bb27131dc7f39c4170db5e56c405f7
Cosette Tshibanda <cosettetshibanda@gmail.com>
2024-01-21 01:29:33 -0700

    set up users controller and model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,32 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  before_action :authorize
+
+  def authorize_user_resource(user_id)
+    render json: { errors: ["You are not authorized to edit this resource"]}, status: :unauthorized unless user_id == current_user.id 
+  end
+
+  def authorized
+    render json: { errors: ["You are already logged in"]}, status: :unauthorized if session.include? :user_id
+  end
+
+  private
+  
+
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+  end
+
+  
+
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity 
+  end
+
 
 end
