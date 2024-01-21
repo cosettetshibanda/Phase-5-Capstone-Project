809e24d8b1c2bd5c6e33fb4b426d3475b3e29018
Cosette Tshibanda <cosettetshibanda@gmail.com>
2024-01-21 02:01:04 -0700

    question controller set up

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,37 @@
 class QuestionsController < ApplicationController
+    before_action :find_question, only: [:show, :update, :destroy]
+    skip_before_action :authorize_user_resource, only: [:index]
+
+    def index
+      if params[:user_id]
+        user = User.find_by_id(params[:user_id])
+        @questions = user.questions 
+      else
+        @questions = Question.all 
+      end
+            render json: @qustions, status: :ok
+    end
+
+    def show
+        render json: @question, status: :ok
+    end
+
+    def update
+        @question.update(question_params)
+        render json: @question, status: :ok
+    end
+
+    def create
+        question = @current_user.questions.create!(review_params)
+        render json: question, status: :created
+    end
+
+    private
+
+    def question_find
+        @question = @current_user.questions.find(params[:id])
+    end
+
+    def question_params
+        params.require(:question).permit(:topic, :post)
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
 
+    has_many :comments, dependent: :destroy
+
     validates :username, presence: true, uniqueness: true
 end
